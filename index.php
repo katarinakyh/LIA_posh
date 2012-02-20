c6c9ecb914e0dd98b5473655ec8324e08b7c7cd2
Johan Schierbeck <johan@schilie.se>
2012-02-20 11:52:55 +0100

    Filstruktur vers1

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8" />
+<head>
+
+  	<title>HTML5-PHP MALL</title>
+  
+  	<meta name="description" content="" />
+ 
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="shortcut icon" href="img/design/favicon.png" />
+  	<link rel="apple-touch-icon" sizes="72x72" href="img/design/apple_touch_57px.png" />
+  	<link rel="apple-touch-icon" sizes="114x114" href="img/design/apple_touch_114px.png" />
+  	
+  	<!--[if lte IE 8]>
+  	<script type="text/javascript" src="js/html5.js"></script>
+  	<![endif]-->
+  	
+  	<!--[if !IE 7]>
+	<style type="text/css">
+		#wrap {display:table;height:100%}
+	</style>
+	<![endif]-->
+	
+  	<script type="text/javascript" src="js/jquery.js"></script>
+  	
+  	</head>
+
+<body>
+
+<div id="wrap">
+
+	<div id="main">
+
+		<nav>
+	    	<ul>
+	    		<li><a href="#">Menu item1</a></li>
+	    		<li><a href="#">Menu item2</a></li>
+	    		<li><a href="#">Menu item3</a></li>
+	    	</ul>  
+    	</nav>
+
+	</div><!-- #main -->
+
+</div><!-- #wrap -->
+
+<div id="footer">
+	STICKY!
+</div><!-- #footer -->
+
+</body>
+</html>
