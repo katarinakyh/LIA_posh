062e4d2898a7a311e2885b3dd278d605da2acb28
Katarina <kgronfors@hotmail.com>
2012-02-20 14:25:27 +0100

    index och style andrade

--- index.php
+++ index.php
@@ -15,18 +15,13 @@
 	<link rel="shortcut icon" href="img/favicon.png" />
   	<link rel="apple-touch-icon" sizes="72x72" href="img/apple_touch_57px.png" />
   	<link rel="apple-touch-icon" sizes="114x114" href="img/apple_touch_114px.png" />
-  	
-  	<!--[if lte IE 8]>
-  	<script type="text/javascript" src="js/html5.js"></script>
-  	<![endif]-->
-  	
-  	<!--[if !IE 7]>
-	<style type="text/css">
-		#wrap {display:table;height:100%}
-	</style>
+	<!--[if !IE 7]>
+		<style type="text/css">
+			#wrap {display:table;height:100%}
+		</style>
 	<![endif]-->
+
 	
-  	<script type="text/javascript" src="js/jquery.js"></script>
   	
   	</head>
 
@@ -36,34 +31,55 @@
 		
 			<div id="main">
 				<header>
-					<div id="logo"> <h2></h>POSH <br /> INDUSTRIES </h2></div>
+					<div id="logo"> <h2> POSH <br /> INDUSTRIES </h2></div>
 					<div id="office"> </div>
 					<div id="socialmedia"> </div>
 				</header>		
 				<section id="mediaclips">
-					<div id="media1"> </div>
-					<div id="media2"> </div>
-					<div id="media3"> </div>
-				
-					<div id="media4"> </div>
-					<div id="media5"> </div>
-					<div id="media6"> </div>
-				
-					<div id="media7"> </div>
-					<div id="media8"> </div>
-					<div id="media9"> </div>
-				
+					<div class="mediagroup" id="group1">
+						<div id="media1"> </div>
+						<div id="media2"> </div>
+						<div id="media3"> </div>
+					</div>
+					<div class="mediagroup" id="group2">
+						<div id="media4"> </div>
+						<div id="media5"> </div>
+						<div id="media6"> </div>
+					</div>
+					<div class="mediagroup" id="group3">
+						<div id="media7"> </div>
+						<div id="media8"> </div>
+						<div id="media9"> </div>
+					</div>
 				</section>
 				
+				<section id="content">
+					<div id="content1"> </div>
+					<div id="content1"> </div>
+					<div id="content1"> </div>
+					<div id="logoimage"> </div>					
+				</section>
+
+				<section id="contact">
+					<div id="address"> </div>
+					<div id="jenni"> </div>
+					<div id="farre"> </div>
+				</section>
 		
 			</div><!-- #main -->
-		
+
+			<footer>
+				<p>&copy; <?php echo date('Y'); ?> POSH INDUSTRIES LTD.</p> 
+			</footer>		
 		</div><!-- #wrap -->
 		
 		<div id="footer">
 			STICKY!
 		</div><!-- #footer -->
-		
-<script src="<?php echo bloginfo('template_url'); ?>/js/less-1.2.1.min.js" type="text/javascript"></script>
+	  	<script type="text/javascript" src="<?php echo bloginfo('template_url'); ?>/js/jquery.js"></script>
+		<script src="<?php echo bloginfo('template_url'); ?>/js/less-1.2.1.min.js" type="text/javascript"></script>
+	  	<!--[if lte IE 8]>
+			<script type="text/javascript" src="js/html5.js"></script>
+		<![endif]-->
 	</body>
 </html>
